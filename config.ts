1d4e0441145214cec217b0eaf5e0ae0af27977af
bickertonav <101204679+bickertonav@users.noreply.github.com>
2022-03-08 18:53:43 -0500

    Update config.ts

--- config.ts
+++ config.ts
@@ -8,10 +8,8 @@ export const CONFIG = {
   //
   // THESE NEXT SETTINGS ARE FOR ADVANCED USERS
   //
-  googleAnalytics: '', // You can use this if you use Google Analytics
   shuffle: false, // whether to shuffle the words in the wordlist each time you load the app (note: you will lose the 'word of the day' functionality if this is true)
   normalization: 'NFC', // whether to apply Unicode normalization to words and orthography - options: 'NFC', 'NFD', 'NKFC', 'NKFD', false
   startDate: 'January 1, 2022 00:00:00', // what date and time to start your game from
   defaultLang: 'en', // the default interface language
-  availableLangs: ['en'], // the options available to the user for translation languages
 }
